f2f2c7b3c6087bcd53ba54244fcb4faacd5dfeb7
cecilDev001 <cecilspencer50@gmail.com>
2024-04-04 17:15:46 +0300

    changes before working on register

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,3 +1,31 @@
+import { Link } from "react-router-dom";
+
 export default function NotFound() {
-  return <div className="min-h-screen">NotFound</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black/10">
+      <div className="flex items-center">
+        <div>
+          <img src="/tri.png" alt="" />
+        </div>
+        <div className="flex gap-5">
+          <div>
+            <h1 className="text-6xl font-semibold">404</h1>
+          </div>
+          <div>
+            <h1 className="text-xl">Page Not Found</h1>
+            <p className="mb-1">
+              The resource you tried to access is not a registered route in this
+              application!
+            </p>
+            <Link
+              to="/"
+              className="py-1 text-white bg-appRed px-4 shadow-md hover:shadow-sm"
+            >
+              Back to Home Page
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
